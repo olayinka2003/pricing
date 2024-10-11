df104524a73419f2dfb5378c0ee59eb3079b4480
olayinka <olayinkaolodude5@gmail.com>
2024-10-11 01:17:28 +0100

    media query

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -1,20 +1,44 @@
-import React from "react";
+import { GiCancel } from "react-icons/gi"; 
+import { GiHamburgerMenu } from "react-icons/gi";
+
+import React, { useState } from "react";
 
 export const Nav = () => {
+  const [active, setIsActive] = useState(false);
+    const handleClick = ()=>{
+      setIsActive(fa=>!fa);
+    }
   return (
-    <nav>
-      <div>
-        <img src="Home.svg" alt="" /> <p>HOMEK</p>
-      </div>
+    <>
+      <nav>
+        <div>
+          <img src="Home.svg" alt="" /> <p>HOMEK</p>
+        </div>
+        <div className="goo">
+          <li>About</li>
+          <li>Speakers</li>
+          <li>Tickets</li>
+          <li>Membership</li>
+        </div>
 
-      <div className="goo">
+        <button>
+          Get your ticket <img src="arrow.svg" alt="" />
+        </button>
+        <GiHamburgerMenu onClick={handleClick} className="menu" />
+      </nav>
+
+    { active &&  <div className="sidebar">
+        <GiCancel onClick={handleClick} className="cancel"/>
+        <ul>
         <li>About</li>
         <li>Speakers</li>
         <li>Tickets</li>
         <li>Membership</li>
+        </ul>
+        <button>
+          Get your ticket <img src="arrow.svg" alt="" />
+        </button>
       </div>
-
-      <button>Get your ticket <img src="arrow.svg" alt="" /></button>
-    </nav>
+}    </>
   );
 };
--- src/component/Plan.jsx
+++ src/component/Plan.jsx
@@ -8,7 +8,7 @@ export const Plan = () => {
         <p className='plan'>We Have the Perfect Plan</p>
         <p className='your'> For Your Needs</p>
         <p className='fees'>No contracts. No surprise fees.</p>
-        <div className='annual'><span onClick={()=>setIsActive('year')}className={active=== 'year'? 'year': 'month'}>Yearly</span><span onClick={()=> setIsActive("month")} className={active=== 'month'? 'year': 'month'}>Monthly</span></div>
+        <div className='annual'><span onClick={()=>setIsActive('year')}className={active=== 'year'? 'year': 'month'} >Yearly</span><span onClick={()=> setIsActive("month")} className={active=== 'month'? 'year': 'month'}>Monthly</span></div>
     </div>
   )
 }
